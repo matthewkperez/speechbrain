8e95bb35de596cf0057c2cc05970f43f21b1963d
Matthew Perez <mkperez@matcha.eecs.umich.edu>
2023-05-04 11:31:01 -0400

    matcha AWER

--- AphasiaBank/helper_scripts/supplement_data.py
+++ AphasiaBank/helper_scripts/supplement_data.py
@@ -4,10 +4,14 @@ update data w.o neologistic
 '''
 import os
 import pandas as pd
+from collections import Counter
 
 if __name__ == "__main__":
-    aphasia_wrd_labels = "/z/mkperez/AphasiaBank/kd_updated/Aphasia/wrd_labels"
-    control_wrd_labels = "/z/mkperez/AphasiaBank/kd_updated/Control/wrd_labels"
+    kaldi_dir = "kd_updated_para"
+    aphasia_wrd_labels = f"/y/mkperez/AphasiaBank/{kaldi_dir}/Aphasia/wrd_labels"
+    control_wrd_labels = f"/y/mkperez/AphasiaBank/{kaldi_dir}/Control/wrd_labels"
+    og_data_dir = "/z/mkperez/speechbrain/AphasiaBank/data/no_paraphasia"
+    new_data_dir = f"{og_data_dir}/mtl_paraphasia"
 
     utt2wrd_label = {}
     labels = []
@@ -35,35 +39,58 @@ if __name__ == "__main__":
                 
                 utt2wrd_label[utt_id] = word_label_seq
                 labels+=word_label_seq
-    print(f"labels: {set(labels)}")
+    print(f"labels: {Counter(labels)}")
 
 
     # data dirs for paraphasia
-    og_data_dir = "/z/mkperez/speechbrain/AphasiaBank/data/Duc_process"
-    new_data_dir = f"{og_data_dir}/mtl_paraphasia"
     os.makedirs(new_data_dir,exist_ok=True)
 
     # make new paraphasia csvs
-    for stype in ['train', 'dev','test']:
-        og_file = f"{og_data_dir}/{stype}.csv"
-        df = pd.read_csv(og_file)
-        og_num_samples = df.shape[0]
+    for para_type in ['pn','p','n','multi']:
+        for stype in ['train', 'dev','test']:
+            og_file = f"{og_data_dir}/{stype}.csv"
+            df = pd.read_csv(og_file)
+            og_num_samples = df.shape[0]
 
 
-        paraphasia_list = []
-        drop_cols = []
-        
-        for i,row in df.iterrows():
-            if row['ID'] not in utt2wrd_label or len(row['wrd'].split()) != len(utt2wrd_label[row['ID']]):
-                drop_cols.append(i)
-            else:
-                paraphasia_list.append(" ".join(utt2wrd_label[row['ID']]))
-  
-        # drop rows
-        df = df.drop(drop_cols)
-        df['paraphasia'] = paraphasia_list
+            paraphasia_list = []
+            drop_cols = []
+            aug_para_wrds= []
+            
 
-        new_num_samples = df.shape[0]
 
-        print(f"{stype}: {og_num_samples} -> {new_num_samples}")
-        df.to_csv(f"{new_data_dir}/{stype}.csv")
+            # filter paraphasias
+            for i,row in df.iterrows():
+                if row['ID'] not in utt2wrd_label or len(row['wrd'].split()) != len(utt2wrd_label[row['ID']]):
+                    drop_cols.append(i)
+                else:
+                    paraphasia_list.append(" ".join(utt2wrd_label[row['ID']]))
+
+                    
+                    if para_type=='multi':
+                        aug_para_wrds.append(" ".join([f"{word}/{para}" for word,para in zip(row['wrd'].split(), utt2wrd_label[row['ID']])]))
+                    elif para_type == 'pn':
+                        aug_para_wrds.append(" ".join([f"{word}/C" if para == 'c' else f"{word}/P" for word,para in zip(row['wrd'].split(), utt2wrd_label[row['ID']])]))
+                    elif para_type == 'p':
+                        aug_para_wrds.append(" ".join([f"{word}/P" if para == 'p' else f"{word}/C" for word,para in zip(row['wrd'].split(), utt2wrd_label[row['ID']])]))
+                    elif para_type == 'n':
+                        aug_para_wrds.append(" ".join([f"{word}/P" if para == 'n' else f"{word}/C" for word,para in zip(row['wrd'].split(), utt2wrd_label[row['ID']])]))
+
+
+            
+
+
+    
+            # drop rows
+            df = df.drop(drop_cols)
+            df['paraphasia'] = paraphasia_list
+            df['aug_para'] = aug_para_wrds
+
+
+            new_num_samples = df.shape[0]
+
+            print(f"{stype}: {og_num_samples} -> {new_num_samples}")
+            df.to_csv(f"{new_data_dir}/{stype}_{para_type}.csv")
+
+
+
